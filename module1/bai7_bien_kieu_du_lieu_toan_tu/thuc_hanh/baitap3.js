0af8d1beae3bc2836cbe3b2e4ff0c14185e3c06e
huyduong155 <nhathuy.155@gmail.com>
2021-03-05 15:05:51 +0700

    update

---
+++ module1/bai7_bien_kieu_du_lieu_toan_tu/thuc_hanh/baitap3.js
@@ -0,0 +1,10 @@
+
+function bai(){
+    let a = prompt('Nhập 2 số')
+    let b = prompt('Nhập 2 số')
+    let c = (a % b) == 0;
+
+    alert('c = '+ c)
+}
+
+
---
+++ module1/bai7_bien_kieu_du_lieu_toan_tu/thuc_hanh/dientich.js
@@ -0,0 +1,7 @@
+    let width = prompt('Nhập Chiều Rộng')
+    let height = prompt('NHập Chiều Cao')
+    let width1 = parseInt(width)
+    let height1 = parseInt(height)
+    let area = width1 * height1
+    document.write('Diện tích là = '+ area)
+
